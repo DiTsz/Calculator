25a86237647e7615df15208f2dd1a8ba776eb14e
Дмитро Цибань <dmytro.tsyban@gmail.com>
2023-06-13 22:16:05 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "1) Enter 1st number;\n" +
+                "2) Enter 2nd number;\n" +
+                "3) Choose the operation (+, -, *, /, %)");
+            Console.WriteLine();
+
+            double a = EnterNum();
+            double b = EnterNum();
+
+            char symbol = Convert.ToChar(Console.ReadLine());
+            switch (symbol)
+            {
+                case '+':
+                    Console.WriteLine($"{a + b}");
+                    break;
+                case '-':
+                    Console.WriteLine($"{a - b}");
+                    break;
+                case '*':
+                    Console.WriteLine($"{a * b}");
+                    break;
+                case '/':
+                    Console.WriteLine($"{a / b}");
+                    break;
+                case '%':
+                    Console.WriteLine($"{a % b}");
+                    break;
+                default:
+                    Console.WriteLine("Wrong operation");
+                    break;
+            }
+            
+
+            double EnterNum()
+            {
+                double num = Convert.ToDouble(Console.ReadLine());
+                return num;
+            }
+        }
+    }
+}
